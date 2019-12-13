4677ec0e20597a31ae56c29a15a7ad6b289b7e21
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-12-13 18:33:42 +0530

    manage template component creation

---
+++ src/app/modules/template/components/manage/manage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss']
+})
+export class ManageTemplateComponent {
+}
