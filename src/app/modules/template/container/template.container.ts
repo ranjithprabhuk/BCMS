3cbaa59ab880dffeac3f99afce250483c224e896
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-12-13 18:34:13 +0530

    template container added

---
+++ src/app/modules/template/container/template.container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-container',
+  templateUrl: './template.container.html',
+  styleUrls: ['./template.container.scss']
+})
+export class TemplateContainer {
+}
