f88c6eb8a2bebaa092ce51c122a11022a353a128
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-12-13 18:35:06 +0530

    refactored the template module routes

--- src/app/modules/template/template.module.ts
+++ src/app/modules/template/template.module.ts
@@ -4,17 +4,36 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { TemplateComponent } from './components/dashboard/template.component';
 import { TemplateService } from './template.service';
+import { TemplateContainer } from './container/template.container';
+import { ManageTemplateComponent } from './components/manage/manage.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: TemplateComponent
+    component: TemplateContainer,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+      },
+      {
+        path: 'dashboard',
+        component: TemplateComponent
+      },
+      {
+        path: 'manage',
+        component: ManageTemplateComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
   declarations: [
-    TemplateComponent
+    TemplateContainer,
+    TemplateComponent,
+    ManageTemplateComponent
   ],
   imports: [
     CommonModule,
