951380d679935b1fe9db42af4a533095a334a385
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2019-12-14 06:49:55 +0530

    chart implementation with mock data

--- src/app/modules/analytics/components/dashboard/analytics.component.ts
+++ src/app/modules/analytics/components/dashboard/analytics.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AnalyticsService } from '../../analytics.service';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-analytics',
@@ -12,6 +12,7 @@ import { Label } from 'ng2-charts';
 export class AnalyticsComponent {
   public barChartOptions: ChartOptions = {
     responsive: true,
+
   };
   public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
@@ -19,8 +20,36 @@ export class AnalyticsComponent {
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Total Mails' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Mails Opened' },
+    // { data: [23, 56, 23, 10, 71, 12, 20], label: 'Links Clicked' },
+  ];
+
+  public lineChartColors: Color[] = [
+    { // red
+      backgroundColor: 'rgba(89,49,150,1)',
+      borderColor: 'white',
+      pointBackgroundColor: 'rgba(89,49,150,0.5)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(89,49,150,0.7)'
+    },
+    { // red
+      backgroundColor: 'rgba(19,185,85,1)',
+      borderColor: 'white',
+      pointBackgroundColor: 'rgba(19,185,85,0.5)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(19,185,85,0.7)'
+    },
+    { // red
+      backgroundColor: 'rgba(239,163,29,1)',
+      borderColor: 'white',
+      pointBackgroundColor: 'rgba(239,163,29,0.5)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(239,163,29,0.7)'
+    },
   ];
 
   constructor(private router: Router, private analyticsService: AnalyticsService) { }
